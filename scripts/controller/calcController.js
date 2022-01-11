8cee3c02f2eff0911104f6f8d18684a447f38b86
Nailton <51789882+nailtonvital@users.noreply.github.com>
2022-01-11 15:47:53 -0300

    audio

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,8 @@
 class CalcController {
 
     constructor() {
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -45,10 +47,28 @@ class CalcController {
 
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn=> {
+            btn.addEventListener('dblclick', e=>{
+                this.toggleAudio();
+            });
+        });
     }
     
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff;
+    };
+
+    playAudio(){
+        if (this._audioOnOff){
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
+    }
+
     initKeyboard(){
         document.addEventListener("keyup", e=>{
+            this.playAudio();
             switch (e.key) {
                 case 'Escape':
                     this.clearAll();
@@ -234,7 +254,7 @@ class CalcController {
     }
 
     execBtn(value) {
-
+        this.playAudio();
         switch (value) {
             case 'ac':
                 this.clearAll();
